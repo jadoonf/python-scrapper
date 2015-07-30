404868b8628517741d7306a69c39e1dc44f01cfc
fkjadoon94 <fkjadoon94@gmail.com>
2015-07-30 02:48:27 -0700

    initial

--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,21 @@
-import scrapy
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['http://blog.scrapinghub.com']
+import re, urllib
 
-    def parse(self, response):
-        for url in response.css('ul li a::attr("href")').re(r'.*/\d\d\d\d/\d\d/$'):
-            yield scrapy.Request(response.urljoin(url), self.parse_titles)
+#If we want to use myurl (the real link) then we will have to append the pre string to get the real url
+myurl = "https://en.wikipedia.org/wiki/List_of_high_schools_in_Virginia"
+#mylocal was created in MSWord by copy pasting the desired links
+mylocal = "file:///Users/bdcoe/Desktop/school_links.htm"
 
-    def parse_titles(self, response):
-        for post_title in response.css('div.entries > ul > li a::text').extract():
-            yield {'title': post_title}
+for i in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(mylocal).read(), re.I):
+        #pre="http://en.wikipedia.org"
+	#fullurl = pre + i;
+	#print fullurl  
+        print "#Opening:\t" + i
+	try:
+		for gg in re.findall('\d\d\d\d\d',re.findall('postal-code">\d\d\d\d\d', urllib.urlopen(i).read(), re.I)[0]):
+			print gg
+
+	#	for ee in re.findall('\d\d\d\d\d', urllib.urlopen(i).read(), re.I):
+        #	        print ee
+	except:
+		print "lun ho gaya"
